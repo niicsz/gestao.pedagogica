7de962e54b207c6d90588b6a2b6f7864863f4858
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-10-22 15:42:44 -0300

    Add files via upload

---
+++ pedagogica/src/main/java/com/gestao/pedagogica/model/SistemaGestaoPedagogicaApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.pedagogica.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoPedagogicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoPedagogicaApplication.class, args);
+	}
+
+}
